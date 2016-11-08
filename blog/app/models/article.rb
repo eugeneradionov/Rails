3f216975d2ac5dcbf3c64b66d972e771ea85bfd8
Eugene Radionov <eugene.radionov@gmail.com>
2016-11-08 10:29:05 +0200

    Initial commit

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-
-  validates :title, presence: true,
+  validates :title, :text, presence: true,
             length: {minimum: 5}
+  validates_associated :comments
 end
